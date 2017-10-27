e6b7264592096c92ca134c73ebce9625716aeb24
Ryan Blecher <notryanb@gmail.com>
2017-10-27 14:01:16 -0400

    Continue working on db example

--- src/proto-db.rs
+++ src/proto-db.rs
@@ -1,3 +1,21 @@
+use tokio_minihttp::{Request, Response};
+
+struct Server {
+    thread_pool: CpuPool,
+    db_pool: r2d2::Pool<r2d2_postgres::PostgresConnectionManager>,
+}
+
+impl Service for Server {
+    type Request = Request;
+    type Response = Response;
+    type Error = io::Error;
+    type Future = Box<Future<Item = Response, Error = io::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        // ...
+    }
+}
+
 fn main () {
     let adder = "127.0.0.1:8080".parse().unwrap();
     let thread_pool = CpuPool::new(10);
@@ -8,6 +26,9 @@ fn main () {
     let db_pool = r2d2::Pool::new(db_config, db_manager).unwrap();
 
     TcpServer::new(tokio_minihttp::Http, addr).serve(move || {
-        // ...
+        Ok(Server {
+            thread_pool: thread_pool.clone(),
+            db_pool: db_pool.clone(),
+        })
     })
 }
